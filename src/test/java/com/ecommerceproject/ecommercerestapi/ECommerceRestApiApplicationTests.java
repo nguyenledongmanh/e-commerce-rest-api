56fca56d2136e04d182441661912c9b087711f9b
Nguyen Dong Manh <nguyenle.dongmanh@gmail.com>
2023-10-15 17:23:13 +0700

    initial project

---
+++ src/test/java/com/ecommerceproject/ecommercerestapi/ECommerceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.ecommercerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
