75eb01eb0764a8d46b9f37e652689c92f59e8f9c
Nguyen Dong Manh <nguyenle.dongmanh@gmail.com>
2023-10-15 18:16:10 +0700

    Add Exception

---
+++ src/main/java/com/ecommerceproject/ecommercerestapi/exception/ErrorDetails.java
@@ -0,0 +1,16 @@
+package com.ecommerceproject.ecommercerestapi.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
